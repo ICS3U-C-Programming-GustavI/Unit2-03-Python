c6fc4df735287f02af6da07dbe2ea2569e7f3479
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-03-04 21:29:58 +0000

    Added pizza_cost.py and constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Gustav Ihlenfeld
+# Created on: February 4, 2025
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
